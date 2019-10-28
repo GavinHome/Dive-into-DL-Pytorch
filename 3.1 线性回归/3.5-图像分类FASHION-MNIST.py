efc756588973b2c30128011ee2554f61bfcfc4e5
GavinHome <yangxiaoming512@yeah.net>
2019-10-28 16:04:11 +0800

    add 3.6

--- 3.1 线性回归/3.5-图像分类FASHION-MNIST.py
+++ 3.1 线性回归/3.5-图像分类FASHION-MNIST.py
@@ -9,8 +9,8 @@ sys.path.append("..") # 为了导⼊上层⽬录的d2lzh_pytorch
 import d2lzh_pytorch as d2l
 
 # %%
-mnist_train = torchvision.datasets.FashionMNIST(root='Datasets/FashionMNIST', train=True, download=True, transform=transforms.ToTensor())
-mnist_test = torchvision.datasets.FashionMNIST(root='Datasets/FashionMNIST', train=False, download=True, transform=transforms.ToTensor())
+mnist_train = torchvision.datasets.FashionMNIST(root='Datasets', train=True, download=True, transform=transforms.ToTensor())
+mnist_test = torchvision.datasets.FashionMNIST(root='Datasets', train=False, download=True, transform=transforms.ToTensor())
 print(type(mnist_train))
 print(len(mnist_train), len(mnist_test))
 
