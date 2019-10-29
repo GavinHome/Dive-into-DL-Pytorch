7cc01dd0d702d8ae888b4f62bf1c376798c4e850
GavinHome <yangxiaoming512@yeah.net>
2019-10-29 09:03:02 +0800

    add 3.10

--- 3.1 线性回归/3.10-多层感知机的简洁实现.py
+++ 3.1 线性回归/3.10-多层感知机的简洁实现.py
@@ -1,4 +1,43 @@
 #%%
 import torch
+from torch import nn
+from torch.nn import init
+import numpy as np
+import sys
+sys.path.append("..")
+import d2lzh_pytorch as d2l
+
+#%% [markdown]
+# 1.定义模型
+num_inputs, num_outputs, num_hiddens = 784, 10, 256
+net = nn.Sequential(
+    d2l.FlattenLayer(),
+    nn.Linear(num_inputs, num_hiddens),
+    nn.ReLU(),
+    nn.Linear(num_hiddens, num_outputs),
+)
+
+for params in net.parameters():
+    init.normal_(params, mean=0, std=0.01)
+
+#%% [markdown]
+# 2.训练模型
+batch_size = 256
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size, resize=None, root='Datasets')
+
+loss = torch.nn.CrossEntropyLoss()
+
+optimizer = torch.optim.SGD(net.parameters(), lr=0.5)
+
+num_epochs = 5
+d2l.train_ch3(net, train_iter, test_iter, loss, num_epochs, batch_size, None, None, optimizer)
+
+#%% [markdown]
+# 3.预测
+X, y = iter(test_iter).next()
+true_labels = d2l.get_fashion_mnist_labels(y.numpy())
+pred_labels = d2l.get_fashion_mnist_labels(net(X).argmax(dim=1).numpy())
+titles = [true + '\n' + pred for true, pred in zip(true_labels,pred_labels)]
+d2l.show_fashion_mnist(X[0:9], titles[0:9])
 
 # %%
